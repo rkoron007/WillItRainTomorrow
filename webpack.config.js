12b3f6bccfed0aa9743b961fe7bf2021bb6503de
Rose <rkoron007@gmail.com>
2018-05-20 12:42:41 -0700

    Set up React

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  entry: "./client/index.js",
+  output: {
+  	filename: "./lib/bundle.js"
+  },
+  module: {
+  loaders: [
+    {
+      test: [/\.jsx?$/, /\.js?$/],
+      exclude: /node_modules/,
+      loader: 'babel-loader',
+      query: {
+        presets: ['env', 'react']
+      }
+    }
+  ]
+},
+  devtool: 'source-map',
+  resolve: {
+    extensions: [".js", ".jsx", "*"]
+  }
+};
